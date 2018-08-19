9f57369122de45cec5dfb6c991f4314b396bf3a3
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2018-08-19 18:15:01 +0530

    project done

--- src/Components/img-component.js
+++ src/Components/img-component.js
@@ -3,42 +3,86 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../App.css';
 import Casey from '../Assests/prague-3540883_640.jpg';
 
 
 class RenderImage extends React.Component{
 
 	constructor(props) {
+
         super(props);
-        this.state = {original_dimensions: {}, set_dimension: {}};
-        this.onImgLoad = this.onImgLoad.bind(this);
-    }
+        this.state 			      = {original_dimensions: {original_height: '', original_width: ''}, set_dimension: { set_height: '', set_width: ''}};
+        this.onImgLoad     		  = this.onImgLoad.bind(this);
+        this.handle_height_change = this.handle_height_change.bind(this);
+        this.handle_width_change  = this.handle_width_change.bind(this);
+
+     }
 
     onImgLoad({target:img}) {
-        this.setState({original_dimensions: { height: img.offsetHeight, width: img.offsetWidth }});
+        this.setState({original_dimensions: { original_height: img.offsetHeight, original_width: img.offsetWidth }});
     }
 
+
+	handle_height_change(event){
+		this.setState({
+		    set_dimension :{ set_height : event.target.value }
+		})
+	}
+	handle_width_change(event){
+		this.setState({
+		    set_dimension :{ set_width : event.target.value }
+		})
+	}
+
+
+
     render(){
-        const {width, height} = this.state.original_dimensions;
+        let {original_width, original_height} = this.state.original_dimensions;
+        let {set_width, set_height} = this.state.set_dimension;
+
+        let final_height = original_height;
+        let final_width  = original_width;
+
+        console.log('wow',this.state);
+        console.log(this);
+
+        if(original_width >= set_width || original_height >= set_height ){
+
+        	final_height = set_height;
+        	final_width  = set_width;
+        }
+
+        const Images_array = [Casey,Casey,Casey,Casey];
+        let _html = Images_array.map( (logo,key)  =>{
+
+        return(
+           <div className="_display_inline" key = {key}>
+                  <img onLoad={this.onImgLoad} src={logo} height = {final_height} width = {final_width}/>
+            </div>
+        )
+      })
+
+
       
         return (
-        	<div className = "container">
+        	<div className = "">
 
-        	 <div className = "col-lg-3">
+        	<form className="form-inline _margin_top"> 
                 <div className="form-group">
-			      <label for="height">Height:</label>
-			      <input type="text" className="form-control"/>
+			      <label htmlFor="height">maxImageHeight:</label>
+			      <input type="text" className="form-control" onChange = {this.handle_height_change} />
 			    </div>
-			    <div className="form-group">
-			      <label for="width">Width:</label>
-			      <input type="text" className="form-control"/>
+
+			    <div className="form-group _margin_right">
+			      <label htmlFor="width">maxImageWidthh:</label>
+			      <input type="text" className="form-control" onChange = {this.handle_width_change}/>
 			    </div>
-			   </div>
-			
-
-
-                <h3>original_dimensions width{width}, height{height}</h3>
-                <img onLoad={this.onImgLoad} src={Casey}/>
+		
+			</form>
+             <div className = "">
+                {_html}
+             </div>
 
              </div>
         );
@@ -46,3 +90,9 @@ class RenderImage extends React.Component{
 }
 
 export default RenderImage;
+
+
+
+
+  // <p>original_dimensions original_width {original_width}, original_height {original_height}</p>
+  //               <p> set_dimension set_width {set_width}, set_height {set_height}  </p>
