47e6bbc0abebb6d755beded01ad88f2ae561675f
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2018-08-19 15:55:24 +0530

    image dimension logic done

--- src/Components/img-component.js
+++ src/Components/img-component.js
@@ -3,27 +3,46 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Casey from '../Assests/casey.jpg';
+import Casey from '../Assests/prague-3540883_640.jpg';
 
 
 class RenderImage extends React.Component{
 
-	constructor(props){
-		super(props)
+	constructor(props) {
+        super(props);
+        this.state = {original_dimensions: {}, set_dimension: {}};
+        this.onImgLoad = this.onImgLoad.bind(this);
+    }
 
-		this.state = {
-			height: 360,
-			width: 360
-		}
-	}
+    onImgLoad({target:img}) {
+        this.setState({original_dimensions: { height: img.offsetHeight, width: img.offsetWidth }});
+    }
 
-	render(){
-		return(
-			<div>
-				<img src= {Casey} alt = "Nature Snapshot"  height = {this.state.heigh} width = {this.state.width}/>
-			</div>
-		)
-	}
+    render(){
+        const {width, height} = this.state.original_dimensions;
+      
+        return (
+        	<div className = "container">
+
+        	 <div className = "col-lg-3">
+                <div className="form-group">
+			      <label for="height">Height:</label>
+			      <input type="text" className="form-control"/>
+			    </div>
+			    <div className="form-group">
+			      <label for="width">Width:</label>
+			      <input type="text" className="form-control"/>
+			    </div>
+			   </div>
+			
+
+
+                <h3>original_dimensions width{width}, height{height}</h3>
+                <img onLoad={this.onImgLoad} src={Casey}/>
+
+             </div>
+        );
+    }
 }
 
 export default RenderImage;
